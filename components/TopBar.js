8f8760620af2a135833ab4f0e656be4fa72fa70e
Jacob Lopez <jacob_m_lopez@berkeley.edu>
2019-06-03 22:18:22 -0700

    top bar with icons still need spacing and shadow

--- components/TopBar.js
+++ components/TopBar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Text, StyleSheet, View, Animated } from 'react-native'
 import { LinearGradient } from 'expo'
 import { Block } from '../components/Block'
+import { Ionicons } from '@expo/vector-icons';
 import * as theme  from '../constants/theme'
 
 export default class TopBar extends Component {
@@ -12,7 +13,9 @@ export default class TopBar extends Component {
                     colors={[theme.colors.primary, theme.colors.accent]}
                     style={styles.title}
                     start={[0, 0]} end={[1, 0]}>
-                    <Text style={styles.text}>Home</Text>
+                        <Ionicons name="md-menu" size={32} color={theme.colors.white} />
+                        <Text style={styles.text}>Home</Text>
+                        <Ionicons name="md-person" size={32} color={theme.colors.white} />
                 </LinearGradient>
             </View>
         );
@@ -20,12 +23,12 @@ export default class TopBar extends Component {
 }
 const styles = StyleSheet.create({
         container: {
-            flexDirection: 'row',
             height: theme.sizes.titleBarHeight, 
         },
         title: {
             flex: 1.0,
-            justifyContent: 'center',
+            flexDirection: 'row',
+            alignItems: 'center',
             paddingLeft: theme.sizes.padding,
             paddingRight: theme.sizes.padding,
             
